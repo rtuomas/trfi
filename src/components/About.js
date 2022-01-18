44351736aef4bbe98647961891f64871a369dd9a
Tuomas Rajala <tuomas.a.rajala@gmail.com>
2022-01-18 22:14:49 +0200

    quick fix

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <div>
         <h1>Welcome to my homepage!</h1>
         <p>I'm a software development student at Metropolia UAS...</p>
-        <p> I've listed some of my projects here.</p>
+        <p>I've listed some of my projects here.</p>
         <p>Feel free to check them out! :)</p>
         <p>Notice! This webpage is not ready yet, be merciful!</p>
       </div>
